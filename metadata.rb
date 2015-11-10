6de610d6e0e32e6910f655ebf5ee4b4dc95a9bcf
Michal Gubik <michal.gubik@burberry.com>
2015-11-10 14:37:47 +0000

    fixed depreceated bash command block

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'fraser.scott@gmail.com'
 license          'MIT'
 description      'Installs/Configures harden'
 long_description 'Installs/Configures harden'
-version          '0.4.4'
+version          '0.4.5'
 
 depends 'apt', '>= 2.6'
 depends 'sensu_spec', '>= 0.10'
